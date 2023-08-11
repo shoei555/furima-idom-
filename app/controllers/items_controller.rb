70ca6e742ffdaa78fc8b23282c5d90c0b48e8d5c
shoei555 <masahide.takakura@glv.co.jp>
2023-08-11 11:45:34 +0900

    出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,34 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+
   def index
 
   end
 
   def new
+    @item = Item.new
+  end
 
+  def create
+    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+# unprocessable_entity」が、発生したエラーの種類を示すもので、「何らかの処理に失敗した」という汎用的な指定方法になります
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_summary, :item_category_id, :item_condition_id, :shipping_cost_id, :prefecture_id, :day_to_shipment_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,23 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :day_to_shipment
+
+  validates :item_name,         presence: true
+  validates :item_summary,      presence: true
+  validates :image,             presence: true, blob: { content_type: :image }
+  validates :price,             presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
+  validates :item_category_id,  numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_cost_id,  numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id,     numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :day_to_shipment_id,numericality: { other_than: 1 , message: "can't be blank"} 
 end
